328cd7966fbc08564f968233c7f08438d9dee3c4
Andres Herrera <18634968+otherandy@users.noreply.github.com>
2022-11-06 12:21:00 -0800

    cors options

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 const app = express();
 require("./database");
 // middlewares
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200,
+}));
 app.use(express.json());
 // routes
 app.use("/api/users", require("./routes/users"));
